5206315979a81f76f4425573b710278e34d05dd3
Chirag Lal Chandani <chiraglalchandani47@gmail.com>
2025-01-05 15:08:24 +0530

    initial commit

---
+++ backend/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://chiraglalchandani47:TutedudeDB@cluster0.btwjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret: "faslkfocvneofu"
+
+};
